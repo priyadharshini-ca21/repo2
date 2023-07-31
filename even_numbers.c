62efa2ac4860d10557a824fe640d566d48822ead
priyadharshini-ca21 <priyadharshinir.ca21@kascsathy.ac.in>
2023-07-31 08:58:04 +0000

    first commit

---
+++ even_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+   {
+     int a;
+     printf("Enter the number:");
+     scanf("%d",&a);
+     if(a%2==0)
+       {
+         printf("a is even number\n");
+       }
+     else
+       {
+         printf("a is odd number\n");
+       }
+    return 0;
+   }
