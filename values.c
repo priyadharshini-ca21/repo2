b6338bdb9a98210e5127cbce4db313c701b6d9df
priyadharshini-ca21 <priyadharshinir.ca21@kascsathy.ac.in>
2023-07-31 09:17:38 +0000

    first commit

---
+++ values.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int main(){
+     inf a,b;
+     printf("enter two values saparated by sape");
+     scanf("%f%f%f%("%d%d%",&a,&a,&b);
+     printf("the entered integer numbers are is %d%d/n",a,b);
+     return 0;
+}
